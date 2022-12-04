45ee6345c25ad7d73d2678e3c29c9986ff916e5c
DeveloperKaleb <kalajholt@gmail.com>
2022-12-03 18:14:40 -0600

    Extended colorchange functionality to more buttons

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ let colorFour = "white";
 
 function renderButtons(pOne = colorOne, pTwo = colorTwo, pThree = colorThree, pFour = colorFour) {
   buttonSpace.innerHTML = `
-  <button class="butOne" style="background-color: ${pOne}"></button>
-  <button class="butTwo" style="background-color: ${pTwo}"></button>
-  <button class="butThree" style="background-color: ${pThree}"></button>
-  <button class="butFour" style="background-color: ${pFour}"></button>
+  <div>
+    <button class="butOneRowOne" style="background-color: ${pOne}"></button>
+    <button class="butTwoRowOne" style="background-color: ${pTwo}"></button>
+    <button class="butThreeRowOne" style="background-color: ${pThree}"></button>
+    <button class="butFourRowOne" style="background-color: ${pFour}"></button>
+  </div>
+  <div>
+    <button class="butOneRowTwo" style="background-color: ${pOne}"></button>
+    <button class="butTwoRowTwo" style="background-color: ${pTwo}"></button>
+    <button class="butThreeRowTwo" style="background-color: ${pThree}"></button>
+    <button class="butFourRowTwo" style="background-color: ${pFour}"></button>
+  </div>
   `;
 }
 renderButtons();
@@ -28,12 +36,20 @@ buttonSpace.addEventListener("click", (event) => {
   if (event.target.className === "butTwo") {
     one = !one;
     three = !three;
+    one ? (colorOne = 'white') : (colorOne = 'black');
+    three ? (colorThree = 'white') : (colorThree = 'black');
+    renderButtons(colorOne, colorTwo, colorThree);
   }
   if (event.target.className === "butThree") {
     two = !two;
     four = !four;
+    two ? (colorTwo = "white") : (colorTwo = "black");
+    four ? (colorFour = 'white') : (colorFour = 'black');
+    renderButtons(colorOne, colorTwo, colorThree, colorFour);
   }
   if (event.target.className === "butFour") {
     three = !three;
+    three ? (colorThree = 'white') : (colorThree = 'black');
+    renderButtons(colorOne, colorTwo, colorThree);
   }
 });
