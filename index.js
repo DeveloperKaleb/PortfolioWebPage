cc801158239fc6663becae7fa0a267527b5a2f54
DeveloperKaleb <kalajholt@gmail.com>
2022-12-04 21:39:36 -0600

    Changed renderButtons function to be customizable

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ let colorTwo = "white";
 let colorThree = "white";
 let colorFour = "white";
 
-function renderButtons(pOne = colorOne, pTwo = colorTwo, pThree = colorThree, pFour = colorFour) {
+/**function renderButtons(pOne = colorOne, pTwo = colorTwo, pThree = colorThree, pFour = colorFour) {
   buttonSpace.innerHTML = `
   <div>
     <button class="butOneRowOne" style="background-color: ${pOne}"></button>
@@ -31,7 +31,38 @@ function renderButtons(pOne = colorOne, pTwo = colorTwo, pThree = colorThree, pF
     <button class="butThreeRowTwo" style="background-color: ${pThree}"></button>
     <button class="butFourRowTwo" style="background-color: ${pFour}"></button>
   </div>
+  <div>
+    <button class="butOneRowOne" style="background-color: ${pOne}"></button>
+    <button class="butTwoRowOne" style="background-color: ${pTwo}"></button>
+    <button class="butThreeRowOne" style="background-color: ${pThree}"></button>
+    <button class="butFourRowOne" style="background-color: ${pFour}"></button>
+  </div>
+  <div>
+    <button class="butOneRowTwo" style="background-color: ${pOne}"></button>
+    <button class="butTwoRowTwo" style="background-color: ${pTwo}"></button>
+    <button class="butThreeRowTwo" style="background-color: ${pThree}"></button>
+    <button class="butFourRowTwo" style="background-color: ${pFour}"></button>
+  </div>
   `;
+}**/
+  
+let html = '';
+
+function renderButtons(width, height, html, row = 1, column = 1) {
+  if (row < height) {
+    return html;
+  }
+
+  html.concat(`<div class="y${row}">`)
+  while (column <= width) {
+    html += `<button class="x${column}y${row}"></button>`
+    column += 1;
+  }
+  html.concat('</div>')
+
+  row += 1;
+
+  renderButtons(width, height, html, row);
 }
 renderButtons();
 
