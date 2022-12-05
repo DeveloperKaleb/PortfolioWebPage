7cfc5c90101a37c1cbbd5e8366e907c687b808b5
DeveloperKaleb <kalajholt@gmail.com>
2022-12-04 23:18:35 -0600

    cleaned up file and honed CSS style slightly

--- index.js
+++ index.js
@@ -6,45 +6,6 @@ const colorArray = [
   'red',
   'green'
 ];
-
-let one = true;
-let two = true;
-let three = true;
-let four = true;
-
-let colorOne = "white";
-let colorTwo = "white";
-let colorThree = "white";
-let colorFour = "white";
-
-/**function renderButtons(pOne = colorOne, pTwo = colorTwo, pThree = colorThree, pFour = colorFour) {
-  buttonSpace.innerHTML = `
-  <div>
-    <button class="butOneRowOne" style="background-color: ${pOne}"></button>
-    <button class="butTwoRowOne" style="background-color: ${pTwo}"></button>
-    <button class="butThreeRowOne" style="background-color: ${pThree}"></button>
-    <button class="butFourRowOne" style="background-color: ${pFour}"></button>
-  </div>
-  <div>
-    <button class="butOneRowTwo" style="background-color: ${pOne}"></button>
-    <button class="butTwoRowTwo" style="background-color: ${pTwo}"></button>
-    <button class="butThreeRowTwo" style="background-color: ${pThree}"></button>
-    <button class="butFourRowTwo" style="background-color: ${pFour}"></button>
-  </div>
-  <div>
-    <button class="butOneRowOne" style="background-color: ${pOne}"></button>
-    <button class="butTwoRowOne" style="background-color: ${pTwo}"></button>
-    <button class="butThreeRowOne" style="background-color: ${pThree}"></button>
-    <button class="butFourRowOne" style="background-color: ${pFour}"></button>
-  </div>
-  <div>
-    <button class="butOneRowTwo" style="background-color: ${pOne}"></button>
-    <button class="butTwoRowTwo" style="background-color: ${pTwo}"></button>
-    <button class="butThreeRowTwo" style="background-color: ${pThree}"></button>
-    <button class="butFourRowTwo" style="background-color: ${pFour}"></button>
-  </div>
-  `;
-}**/
   
 let renderHtml = '';
 
@@ -56,7 +17,7 @@ function renderButtons(width, height, html, row = 1, column = 1) {
 
   renderHtml = renderHtml.concat(`<div class="y${row}">`)
   while (column <= width) {
-    renderHtml += `<button class="x${column}y${row}"></button>`
+    renderHtml += `<button class="x${column}y${row}" style="background-color: white"></button>`
     column += 1;
   }
   renderHtml = renderHtml.concat('</div>')
@@ -69,8 +30,7 @@ renderButtons(5, 3, renderHtml);
 buttonSpace.addEventListener("click", (event) => {
   const clickedClass = event.target.className;
   const clickedElement = document.querySelector(`.${clickedClass}`)
-  let buttonBackColor = clickedElement.style.backgroundColor;
-  console.log('eventTarget:', event.target, 'clickedClass:', clickedClass, 'clickedElement:', clickedElement, 'buttonBackColor:', buttonBackColor);
+  const buttonBackColor = clickedElement.style.backgroundColor;
 
   if (buttonBackColor && buttonBackColor === 'white') {
     clickedElement.style.backgroundColor = 'black';
